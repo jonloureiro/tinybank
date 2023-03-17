854c2e7a19aa58a8d58602c54ae04b9cb34797a7
Jonathan Loureiro <19528508+jonloureiro@users.noreply.github.com>
2023-03-17 15:06:26 -0400

    feat: list accounts usecase

---
+++ app/domain/usecases/list_accounts.go
@@ -0,0 +1,18 @@
+package usecases
+
+import "github.com/jonloureiro/tiny-bank/app/domain/entities"
+
+type ListAccountInput struct {
+}
+
+type ListAccountOutput struct {
+	Accounts []*entities.Account
+}
+
+func (uC *TinyBankUseCases) ListAccount(input ListAccountInput) (*ListAccountOutput, error) {
+	accounts, err := uC.AccountsRepo.List()
+	if err != nil {
+		return nil, ErrDatabaseUnknownError
+	}
+	return &ListAccountOutput{Accounts: accounts}, nil
+}
---
+++ app/domain/usecases/list_accounts_test.go
@@ -0,0 +1,56 @@
+package usecases_test
+
+import (
+	"testing"
+
+	"github.com/jonloureiro/tiny-bank/app/domain/entities"
+	"github.com/jonloureiro/tiny-bank/app/domain/usecases"
+	"github.com/jonloureiro/tiny-bank/app/domain/usecases/repositories/mocks"
+	"github.com/jonloureiro/tiny-bank/app/domain/vo"
+)
+
+func TestListAccounts(t *testing.T) {
+	var (
+		validName   = "Test"
+		validSecret = "123456"
+		validCPF, _ = vo.NewCPF("69029890100")
+	)
+	t.Run("list accounts", func(t *testing.T) {
+		accountsRepo := mocks.NewAccountsRepositoryMock()
+		uC := usecases.TinyBankUseCases{AccountsRepo: accountsRepo}
+		account, _ := entities.NewAccount(validName, validCPF, validSecret)
+		_ = uC.AccountsRepo.Create(account)
+
+		output, err := uC.ListAccount(usecases.ListAccountInput{})
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+		}
+
+		if len(output.Accounts) != 1 {
+			t.Errorf("expected 1 account, got %d", len(output.Accounts))
+		}
+	})
+
+	t.Run("validate empty slice", func(t *testing.T) {
+		want := make([]*entities.Account, 0)
+		accountsRepo := mocks.NewAccountsRepositoryMock()
+		uC := usecases.TinyBankUseCases{AccountsRepo: accountsRepo}
+		output, err := uC.ListAccount(usecases.ListAccountInput{})
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+		}
+		if len(output.Accounts) != len(want) {
+			t.Errorf("expected %d accounts, got %d", len(want), len(output.Accounts))
+		}
+	})
+
+	t.Run("validate error", func(t *testing.T) {
+		accountsRepo := mocks.NewAccountsRepositoryMock()
+		accountsRepo.UnknownError = true
+		uC := usecases.TinyBankUseCases{AccountsRepo: accountsRepo}
+		_, err := uC.ListAccount(usecases.ListAccountInput{})
+		if err != usecases.ErrDatabaseUnknownError {
+			t.Errorf("expected error %v, got %v", usecases.ErrDatabaseUnknownError, err)
+		}
+	})
+}
