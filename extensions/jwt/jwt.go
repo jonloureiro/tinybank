6fe90e23e1b4f2b0f9e8c2b4dd2a49ed599a18cb
Jonathan Loureiro <19528508+jonloureiro@users.noreply.github.com>
2023-03-17 18:14:13 -0400

    feat: jwt

---
+++ extensions/jwt/jwt.go
@@ -0,0 +1,81 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jonloureiro/tiny-bank/extensions/id"
+)
+
+type Payload struct {
+	AccountId id.ID
+}
+
+type Token struct {
+	AccountId      id.ID
+	Token          string
+	ExpirationTime time.Time
+	IssuedAt       time.Time
+}
+
+const (
+	TTL = 15 * time.Minute
+)
+
+var ErrTokenUnknownError = errors.New("token unknown error")
+
+func New(payload *Payload, privateKey string) (*Token, error) {
+	issuedAt := time.Now()
+	expirationTime := issuedAt.Add(TTL)
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"account_id": payload.AccountId,
+		"exp":        expirationTime.Unix(),
+		"iat":        issuedAt.Unix(),
+	})
+	token, err := jwtToken.SignedString([]byte(privateKey))
+	if err != nil {
+		return nil, ErrTokenUnknownError
+	}
+	return &Token{
+		AccountId:      payload.AccountId,
+		Token:          token,
+		ExpirationTime: expirationTime,
+		IssuedAt:       issuedAt,
+	}, nil
+}
+
+func Parse(token, privateKey string) (*Token, error) {
+	t, err := jwt.ParseWithClaims(token, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(privateKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := t.Claims.(*jwt.MapClaims)
+	if !ok {
+		return nil, ErrTokenUnknownError
+	}
+	accountId, ok := (*claims)["account_id"].(string)
+	if !ok {
+		return nil, ErrTokenUnknownError
+	}
+	expirationTime, ok := (*claims)["exp"].(float64)
+	if !ok {
+		return nil, ErrTokenUnknownError
+	}
+	issuedAt, ok := (*claims)["iat"].(float64)
+	if !ok {
+		return nil, ErrTokenUnknownError
+	}
+	return &Token{
+		AccountId:      id.ID(accountId),
+		Token:          token,
+		ExpirationTime: time.Unix(int64(expirationTime), 0),
+		IssuedAt:       time.Unix(int64(issuedAt), 0),
+	}, nil
+}
+
+func (t *Token) IsExpired() bool {
+	return t.ExpirationTime.Before(time.Now())
+}
---
+++ extensions/jwt/jwt_test.go
@@ -0,0 +1,57 @@
+package jwt_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/jonloureiro/tiny-bank/extensions/id"
+	"github.com/jonloureiro/tiny-bank/extensions/jwt"
+)
+
+const (
+	privateKey     = "s3cr3t"
+	fifteenMinutes = 15 * time.Minute
+)
+
+func TestNewToken(t *testing.T) {
+	want := reflect.TypeOf(&jwt.Token{})
+	accountID := id.New()
+	token, err := jwt.New(&jwt.Payload{accountID}, privateKey)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	got := reflect.TypeOf(token)
+	if want != got {
+		t.Errorf("expected %v, got %v", want, got)
+	}
+	if token.AccountId != accountID {
+		t.Errorf("expected %v, got %v", accountID, token.AccountId)
+	}
+	if token.ExpirationTime.Sub(token.IssuedAt) != fifteenMinutes {
+		t.Errorf("expected %v, got %v", fifteenMinutes, token.ExpirationTime.Sub(token.IssuedAt))
+	}
+}
+
+func TestParseToken(t *testing.T) {
+	token, err := jwt.New(&jwt.Payload{id.New()}, privateKey)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	parsedToken, err := jwt.Parse(token.Token, privateKey)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if token.AccountId != parsedToken.AccountId {
+		t.Errorf("[AccountId] expected %v, got %v", token.AccountId, parsedToken.AccountId)
+	}
+	if token.Token != parsedToken.Token {
+		t.Errorf("[AccountId] expected %v, got %v", token.Token, parsedToken.Token)
+	}
+	if token.ExpirationTime.Unix() != parsedToken.ExpirationTime.Unix() {
+		t.Errorf("[ExpirationTime] expected %v, got %v", token.ExpirationTime, parsedToken.ExpirationTime)
+	}
+	if token.IssuedAt.Unix() != parsedToken.IssuedAt.Unix() {
+		t.Errorf("[IssuedAt] expected %v, got %v", token.IssuedAt, parsedToken.IssuedAt)
+	}
+}
