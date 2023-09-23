b1f7c8eabfcfe346fb78282c73da85576873eb13
Jonathan Loureiro <19528508+jonloureiro@users.noreply.github.com>
2023-09-23 11:35:00 -0400

    refactor: move vos to app folder

--- internal/accounts/app/account.go
+++ internal/accounts/app/account.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/jonloureiro/tiny-bank/internal/accounts"
-	"github.com/jonloureiro/tiny-bank/internal/accounts/vos"
+	"github.com/jonloureiro/tiny-bank/internal/accounts/app/vos"
 	"github.com/jonloureiro/tiny-bank/internal/common"
 )
 
